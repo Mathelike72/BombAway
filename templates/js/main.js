c84fc4d222c6757086c76969a2088c038b4a19be
Matteo Miggiano <mmiggiano@me.com>
2020-12-30 17:54:46 +0100

    verbesserung code

--- templates/js/main.js
+++ templates/js/main.js
@@ -153,12 +153,183 @@ const player2 = {
   }
 }
 
+// blow up a bomb and its surrounding tiles
+function blowUpBomb(bomb) {
 
-// Bomb placing (just simple circle)
-const bomb = {
+  // bomb has already exploded so don't blow up again
+  if (!bomb.alive) return;
 
+  bomb.alive = false;
+
+  // remove bomb from grid
+  cells[bomb.row][bomb.col] = null;
+
+  // explode bomb outward by size
+  const dirs = [{
+    // up
+    row: -1,
+    col: 0
+  }, {
+    // down
+    row: 1,
+    col: 0
+  }, {
+    // left
+    row: 0,
+    col: -1
+  }, {
+    // right
+    row: 0,
+    col: 1
+  }];
+
+  dirs.forEach((dir) => {
+    for (let i = 0; i < bomb.size; i++) {
+      const row = bomb.row + dir.row * i;
+      const col = bomb.col + dir.col * i;
+      const cell = cells[row][col];
+
+      // stop the explosion if it hit a wall
+      if (cell === types.wall) {
+        return;
+      }
+
+      // center of the explosion is the first iteration of the loop
+      entities.push(new Explosion(row, col, dir, i === 0 ? true : false));
+      cells[row][col] = null;
+
+      // bomb hit another bomb so blow that one up too
+      if (cell === types.bomb) {
+
+        // find the bomb that was hit by comparing positions
+        const nextBomb = entities.find((entity) => {
+          return (
+            entity.type === types.bomb &&
+            entity.row === row && entity.col === col
+          );
+        });
+        blowUpBomb(nextBomb);
+      }
+
+      // stop the explosion if hit anything
+      if (cell) {
+        return;
+      }
+    }
+  });
 }
 
+// bomb constructor function
+function Bomb(row, col, size, owner) {
+  this.row = row;
+  this.col = col;
+  this.radius = grid * 0.4;
+  this.size = size;    // the size of the explosion
+  this.owner = owner;  // which player placed this bomb
+  this.alive = true;
+  this.type = types.bomb;
+
+  // bomb blows up after 3 seconds
+  this.timer = 3000;
+
+  // update the bomb each frame
+  this.update = function(dt) {
+    this.timer -= dt;
+
+    // blow up bomb if timer is done
+    if (this.timer <= 0) {
+      return blowUpBomb(this);
+    }
+
+    // change the size of the bomb every half second. we can determine the size
+    // by dividing by 500 (half a second) and taking the ceiling of the result.
+    // then we can check if the result is even or odd and change the size
+    const interval = Math.ceil(this.timer / 500);
+    if (interval % 2 === 0) {
+      this.radius = grid * 0.4;
+    }
+    else {
+      this.radius = grid * 0.5;
+    }
+  };
+
+  // render the bomb each frame
+  this.render = function() {
+    const x = (this.col + 0.5) * grid;
+    const y = (this.row + 0.5) * grid;
+
+    // draw bomb
+    context.fillStyle = 'black';
+    context.beginPath();
+    context.arc(x, y, this.radius, 0, 2 * Math.PI);
+    context.fill();
+
+    // draw bomb fuse moving up and down with the bomb size
+    const fuseY = (this.radius === grid * 0.5 ? grid * 0.15 : 0);
+    context.strokeStyle = 'white';
+    context.lineWidth = 5;
+    context.beginPath();
+    context.arc(
+      (this.col + 0.75) * grid,
+      (this.row + 0.25) * grid - fuseY,
+      10, Math.PI, -Math.PI / 2
+    );
+    context.stroke();
+  };
+}
+
+// explosion constructor function
+function Explosion(row, col, dir, center) {
+  this.row = row;
+  this.col = col;
+  this.dir = dir;
+  this.alive = true;
+
+  // show explosion for 0.3 seconds
+  this.timer = 300;
+
+  // update the explosion each frame
+  this.update = function(dt) {
+    this.timer -= dt;
+
+    if (this.timer <=0) {
+      this.alive = false;
+    }
+  };
+
+  // render the explosion each frame
+  this.render = function() {
+    const x = this.col * grid;
+    const y = this.row * grid;
+    const horizontal = this.dir.col;
+    const vertical = this.dir.row;
+
+    // create a fire effect by stacking red, orange, and yellow on top of
+    // each other using progressively smaller rectangles
+    context.fillStyle = '#D72B16';  // red
+    context.fillRect(x, y, grid, grid);
+
+    context.fillStyle = '#F39642';  // orange
+
+    // determine how to draw based on if it's vertical or horizontal
+    // center draws both ways
+    if (center || horizontal) {
+      context.fillRect(x, y + 6, grid, grid - 12);
+    }
+    if (center || vertical) {
+      context.fillRect(x + 6, y, grid - 12, grid);
+    }
+
+    context.fillStyle = '#FFE5A8';  // yellow
+
+    if (center || horizontal) {
+      context.fillRect(x, y + 12, grid, grid - 24);
+    }
+    if (center || vertical) {
+      context.fillRect(x + 12, y, grid - 24, grid);
+    }
+  };
+}
 
 // game loop
 let last;
@@ -244,6 +415,24 @@ document.addEventListener('keydown', function(e) {
     player.row = row;
     player.col = col;
   }
+  // space key (bomb)
+  else if (
+  e.which === 32 && !cells[row][col] &&
+  // count the number of bombs the player has placed
+  entities.filter((entity) => {
+    return entity.type === types.bomb && entity.owner === player
+  }).length < player.numBombs
+) {
+  // place bomb
+  const bomb = new Bomb(row, col, player.bombSize, player);
+  entities.push(bomb);
+  cells[row][col] = types.bomb;
+  }
+  // don't move the player if something is already at that position
+  if (cells[row][col] !== types.wall && cells[row][col] !== types.swall && cells[row][col] !== types.bomb) {
+    player.row = row;
+    player.col = col;
+  }
 });
 
 // listen to keyboard events to move the player
@@ -273,6 +462,23 @@ document.addEventListener('keydown', function(e) {
   }
   // don't move the player if something is already at that position
   if (template.cells[row][col] !== types.wall && template.cells[row][col] !== types.swall) {
+    player2.row = row;
+    player2.col = col;
+  }
+  // space key (bomb)
+  else if (
+  e.which === 16 && !cells[row][col] &&
+  // count the number of bombs the player has placed
+  entities.filter((entity) => {
+    return entity.type === types.bomb && entity.owner === player
+  }).length < player.numBombs) {
+  // place bomb
+  const bomb = new Bomb(row, col, player.bombSize, player);
+  entities.push(bomb);
+  cells[row][col] = types.bomb;
+  }
+  // don't move the player if something is already at that position
+  if (cells[row][col] !== types.wall && cells[row][col] !== types.swall && cells[row][col] !== types.bomb)  {
     player2.row = row;
     player2.col = col;
   }
